1b7200027166803a2e1bf83d46d26f05bad8e009
Duncan Baldwin <124105688+DuncanKoji@users.noreply.github.com>
2023-03-11 13:48:21 -0700

    Update monthkeeper.js

--- date/monthkeeper.js
+++ date/monthkeeper.js
@@ -4,6 +4,19 @@ window.onload = function() {
   var dt = new Date();
   var month = dt.getMonth();
   var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    
   ]
   
   switch(month) {
