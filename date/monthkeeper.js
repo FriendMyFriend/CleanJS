826624fae45576ffc8fe654d86d1150a86026d51
Duncan Baldwin <124105688+DuncanKoji@users.noreply.github.com>
2023-03-11 13:55:25 -0700

    Make code cleaner

--- date/monthkeeper.js
+++ date/monthkeeper.js
@@ -18,43 +18,42 @@ window.onload = function() {
     "December"
     
   ]
-  case 0:
+  switch(month) {
+    case 0:
       document.querySelector(".month").innerHTML = months[0];
     break;
-  case 1:
+    case 1:
       document.querySelector(".month").innerHTML = months[1];
     break;
-  case 2:
+    case 2:
       document.querySelector(".month").innerHTML = months[2];
     break;
-  case 3:
+    case 3:
       document.querySelector(".month").innerHTML = months[3];
     break;
-  case 4:
+    case 4:
       document.querySelector(".month").innerHTML = months[4];
     break;
-  case 5:
+    case 5:
       document.querySelector(".month").innerHTML = months[5];
     break;
-  case 6:
+    case 6:
       document.querySelector(".month").innerHTML = months[6];
     break;
-  case 7:
+    case 7:
       document.querySelector(".month").innerHTML = months[7];
     break;
-  case 8:
+    case 8:
       document.querySelector(".month").innerHTML = months[8];
     break;
-  case 9:
+    case 9:
       document.querySelector(".month").innerHTML = months[9];
     break;
-  case 10:
+    case 10:
       document.querySelector(".month").innerHTML = months[10];
     break;
-  case 11:
+    case 11:
       document.querySelector(".month").innerHTML = months[11];
     break;
-  
-  switch(month) {
   }
 }
