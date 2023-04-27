d927b4e5983ad37b4529440e08befb8d6ab078a5
xt39 <maysuncampos@yahoo.com>
2023-04-27 09:10:10 -0600

    Major updates and clean up's

--- date/daykeeper.js
+++ date/daykeeper.js
@@ -1,46 +1,12 @@
 "use strict";
 
 window.onload = function() {
-  var d = new Date();
-  var day = d.getDay();
-  var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  
-  switch(day) {
-    case 0:
-      document.querySelector(".day").innerHTML = days[0];
-    break;
-    case 1:
-      document.querySelector(".day").innerHTML = days[1];
-    break;
-    case 2:
-      document.querySelector(".day").innerHTML = days[2];
-    break;
-    case 3:
-      document.querySelector(".day").innerHTML = days[3];
-    break;
-    case 4:
-      document.querySelector(".day").innerHTML = days[4];
-    break;
-    case 5:
-      document.querySelector(".day").innerHTML = days[5];
-    break;
-    case 6:
-      document.querySelector(".day").innerHTML = days[6];
-    break;
-    default:
-      setTimeout("retry()", 5000);
-    break;
-  }
-}
+  const d, day, days;
 
-function retry() {
-  window.location.reload();
+  d = new Date();
+  day = d.getDay();
+  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  document.querySelector(".day").innerHTML = day[days];
+  document.querySelector("#day").innerHTML = day[days];
 }
--- date/monthkeeper.js
+++ date/monthkeeper.js
@@ -1,9 +1,11 @@
 "use strict";
 
 window.onload = function() {
-  var dt = new Date();
-  var month = dt.getMonth();
-  var months = [
+  const dt, month, months;
+
+  dt = new Date();
+  month = dt.getMonth();
+  months = [
     "January",
     "February",
     "March",
@@ -17,46 +19,7 @@ window.onload = function() {
     "November",
     "December"
   ];
-  
-  switch(month) {
-    case 0:
-      document.querySelector(".month").innerHTML = months[0];
-    break;
-    case 1:
-      document.querySelector(".month").innerHTML = months[1];
-    break;
-    case 2:
-      document.querySelector(".month").innerHTML = months[2];
-    break;
-    case 3:
-      document.querySelector(".month").innerHTML = months[3];
-    break;
-    case 4:
-      document.querySelector(".month").innerHTML = months[4];
-    break;
-    case 5:
-      document.querySelector(".month").innerHTML = months[5];
-    break;
-    case 6:
-      document.querySelector(".month").innerHTML = months[6];
-    break;
-    case 7:
-      document.querySelector(".month").innerHTML = months[7];
-    break;
-    case 8:
-      document.querySelector(".month").innerHTML = months[8];
-    break;
-    case 9:
-      document.querySelector(".month").innerHTML = months[9];
-    break;
-    case 10:
-      document.querySelector(".month").innerHTML = months[10];
-    break;
-    case 11:
-      document.querySelector(".month").innerHTML = months[11];
-    break;
-    default:
-      // do nothing
-    break;
-  }
+
+  document.querySelector(".month").innerHTML = month[months];
+  document.querySelector("#month").innerHTML = month[months];
 }
