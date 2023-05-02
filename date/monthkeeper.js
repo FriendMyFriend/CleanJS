1747afaba4ae1caa5953fc28813af8c5ce22a0ae
Duncan Baldwin <124105688+DuncanKoji@users.noreply.github.com>
2023-05-02 12:32:26 -0600

    Made the mounth keeper have less lines

--- date/monthkeeper.js
+++ date/monthkeeper.js
@@ -5,20 +5,7 @@ window.onload = function() {
 
   dt = new Date();
   month = dt.getMonth();
-  months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
+  months = [  "January",  "February", "March",  "April",  "May",  "June",  "July",  "August",  "September",  "October",  "November",  "December"];
 
   document.querySelector(".month").innerHTML = month[months];
   document.querySelector("#month").innerHTML = month[months];
